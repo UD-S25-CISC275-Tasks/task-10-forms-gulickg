f76c288500bd4b80033aa98973a46d4768c4d415
Grace <grace.c.gulick@gmail.com>
2025-03-14 14:00:08 -0400

    ///

--- src/nested.ts
+++ src/nested.ts
@@ -1,27 +1,20 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-<<<<<<< HEAD
 import {
     duplicateQuestion,
     makeBlankQuestion,
-    renameQuestion
+    renameQuestion,
 } from "./objects";
-=======
->>>>>>> origin/task-forms
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-<<<<<<< HEAD
     const newQuest = questions.filter(
-        (question: Question): boolean => question.published
+        (question: Question): boolean => question.published,
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /**
@@ -30,17 +23,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-<<<<<<< HEAD
     const newQuest = questions.filter(
         (question: Question): boolean =>
             question.body !== "" ||
             question.expected !== "" ||
-            question.options.length > 0
+            question.options.length > 0,
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -49,17 +38,14 @@ export function getNonEmptyQuestions(questions: Question[]): Question[] {
  */
 export function findQuestion(
     questions: Question[],
-    id: number
+    id: number,
 ): Question | null {
-<<<<<<< HEAD
     const found = questions.find(
-        (question: Question): boolean => question.id == id
+        (question: Question): boolean => question.id == id,
     );
     if (found) {
         return found;
     }
-=======
->>>>>>> origin/task-forms
     return null;
 }
 
@@ -68,14 +54,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-<<<<<<< HEAD
     const newQuest = questions.filter(
-        (question: Question): boolean => question.id != id
+        (question: Question): boolean => question.id != id,
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -83,42 +65,30 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-<<<<<<< HEAD
     const newQuest = questions.map(
-        (question: Question): string => question.name
+        (question: Question): string => question.name,
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-<<<<<<< HEAD
     const sum = questions.reduce(
         (total, question): number => total + question.points,
-        0
+        0,
     );
     return sum;
-=======
-    return 0;
->>>>>>> origin/task-forms
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-<<<<<<< HEAD
     const newQuest = getPublishedQuestions(questions);
     const sum = sumPoints(newQuest);
     return sum;
-=======
-    return 0;
->>>>>>> origin/task-forms
 }
 
 /***
@@ -139,7 +109,6 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-<<<<<<< HEAD
     let newQuest = questions.map(
         (question: Question) =>
             question.id.toString() +
@@ -150,12 +119,9 @@ export function toCSV(questions: Question[]): string {
             "," +
             question.points.toString() +
             "," +
-            question.published.toString()
+            question.published.toString(),
     );
     return "id,name,options,points,published" + "\n" + newQuest.join("\n");
-=======
-    return "";
->>>>>>> origin/task-forms
 }
 
 /**
@@ -164,19 +130,15 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-<<<<<<< HEAD
     const answers = questions.map(
         (question: Question): Answer => ({
             questionId: question.id,
             text: "",
             submitted: false,
-            correct: false
-        })
+            correct: false,
+        }),
     );
     return answers;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -184,17 +146,13 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-<<<<<<< HEAD
     const newQuest = questions.map(
         (question: Question): Question => ({
             ...question,
-            published: true
-        })
+            published: true,
+        }),
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -202,13 +160,9 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-<<<<<<< HEAD
     return questions.every(
-        (question: Question) => question.type == questions[0].type
+        (question: Question) => question.type == questions[0].type,
     );
-=======
-    return false;
->>>>>>> origin/task-forms
 }
 
 /***
@@ -220,15 +174,11 @@ export function addNewQuestion(
     questions: Question[],
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question[] {
-<<<<<<< HEAD
     const newQuest: Question = makeBlankQuestion(id, name, type);
     const copyQuest = [...questions, newQuest];
     return copyQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -239,9 +189,8 @@ export function addNewQuestion(
 export function renameQuestionById(
     questions: Question[],
     targetId: number,
-    newName: string
+    newName: string,
 ): Question[] {
-<<<<<<< HEAD
     const targetQuest = questions.map((question: Question): Question => {
         if (question.id == targetId) {
             const newQuest: Question = renameQuestion(question, newName);
@@ -250,9 +199,6 @@ export function renameQuestionById(
         return question;
     });
     return targetQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -262,17 +208,13 @@ export function renameQuestionById(
  * AND if the `newQuestionType` is no longer "multiple_choice_question" than the `options`
  * must be set to an empty list.
  */
-<<<<<<< HEAD
 
 //error
-=======
->>>>>>> origin/task-forms
 export function changeQuestionTypeById(
     questions: Question[],
     targetId: number,
-    newQuestionType: QuestionType
+    newQuestionType: QuestionType,
 ): Question[] {
-<<<<<<< HEAD
     const newQuest = questions.map((question: Question): Question => {
         if (question.id == targetId) {
             question.type == newQuestionType;
@@ -284,9 +226,6 @@ export function changeQuestionTypeById(
         return question;
     });
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
 
 /**
@@ -303,8 +242,7 @@ export function editOption(
     questions: Question[],
     targetId: number,
     targetOptionIndex: number,
-    newOption: string
-<<<<<<< HEAD
+    newOption: string,
 ): Question[] {
     const newQuest = questions.map((question: Question): Question => {
         if (question.id == targetId) {
@@ -319,10 +257,6 @@ export function editOption(
         return question;
     });
     return newQuest;
-=======
-) {
-    return [];
->>>>>>> origin/task-forms
 }
 
 /***
@@ -334,20 +268,16 @@ export function editOption(
 export function duplicateQuestionInArray(
     questions: Question[],
     targetId: number,
-    newId: number
+    newId: number,
 ): Question[] {
-<<<<<<< HEAD
     const newQuest = [...questions];
     const questIndex: number = newQuest.findIndex(
-        (question: Question): boolean => question.id == targetId
+        (question: Question): boolean => question.id == targetId,
     );
     newQuest.splice(
         questIndex + 1,
         0,
-        duplicateQuestion(newId, questions[questIndex])
+        duplicateQuestion(newId, questions[questIndex]),
     );
     return newQuest;
-=======
-    return [];
->>>>>>> origin/task-forms
 }
